c3f316d2b77c1a04e5b65b21b7b84b36db260002
Lucas Rocha <ld.rocha@hotmail.com>
2020-12-08 20:08:03 -0800

    refactoring project to use class instead of plain constructors

--- library.js
+++ library.js
@@ -7,11 +7,13 @@ const authorTextField = document.getElementById('author');
 const pagesTextField = document.getElementById('Pages');
 const checkbox = document.getElementById('status');
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 
 loadData();
